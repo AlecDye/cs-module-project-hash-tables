37542d147fc92f56369b2d0752db1db0bb4ecc62
alec dye <alecdye@gmail.com>
2020-08-05 16:08:41 -0700

    Complete lookup table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,21 +1,39 @@
+import math
+import random
+
 # Your code here
+
+# output example: 49999: 5,5: None
 
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
     v = math.factorial(v)
-    v //= (x + y)
+    v //= x + y
     v %= 982451653
 
     return v
+
+
+# storage
+cache = {}
+
 
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
+    if (x, y) not in cache:
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= x + y
+        v %= 982451653
+        cache[x, y] = v
 
+    return cache[(x, y)]
+
+    # not sure if this is faster?
 
 
 # Do not modify below this line!
@@ -23,4 +41,4 @@ def slowfun(x, y):
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
-    print(f'{i}: {x},{y}: {slowfun(x, y)}')
+    print(f"{i}: {x},{y}: {slowfun(x, y)}")
